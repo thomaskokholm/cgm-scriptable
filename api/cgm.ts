8284e2c063bdce083e47d9460a3b1517d1a59a44
Thomas Kokholm <thomas@kokholm.org>
2023-11-05 21:04:06 +0100

    Update result

--- api/cgm.ts
+++ api/cgm.ts
@@ -11,24 +11,24 @@ export async function GET() {
       values: [],
     });
     console.log("result", result);
+    return new Response(
+      JSON.stringify({
+        data: {
+          id: 226,
+          time: "2023-11-05T18:19:27.000Z",
+          val: 7.4,
+          trend: "Flat",
+          results: JSON.stringify(result),
+        },
+      }),
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
   } catch (error) {
     console.log(error);
   }
-
-  return new Response(
-    JSON.stringify({
-      data: {
-        id: 226,
-        time: "2023-11-05T18:19:27.000Z",
-        val: 7.4,
-        trend: "Flat",
-      },
-    }),
-    {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-    }
-  );
 }
