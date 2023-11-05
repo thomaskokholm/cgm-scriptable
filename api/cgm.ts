7124938200782e6b49ee8a43777929c925be423e
Thomas Kokholm <thomas@kokholm.org>
2023-11-05 21:12:30 +0100

    Update to get only one

--- api/cgm.ts
+++ api/cgm.ts
@@ -3,27 +3,16 @@ import excuteQuery from "../lib/db";
 export async function GET() {
   try {
     const result = await excuteQuery({
-      query: "SELECT * FROM `glucose` ORDER BY `id` DESC",
+      query: "SELECT * FROM `glucose` ORDER BY `id` DESC LIMIT 1",
       values: [],
     });
     console.log("result", result);
-    return new Response(
-      JSON.stringify({
-        data: {
-          id: 226,
-          time: "2023-11-05T18:19:27.000Z",
-          val: 7.4,
-          trend: "Flat",
-          results: JSON.stringify(result),
-        },
-      }),
-      {
-        status: 200,
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    );
+    return new Response(JSON.stringify(result), {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
   } catch (error) {
     console.log(error);
   }
