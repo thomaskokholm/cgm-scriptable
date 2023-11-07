0af8b0771fd662fb093b069efba3286ca9fffb34
Thomas Kokholm <thomas@kokholm.org>
2023-11-07 20:00:20 +0100

    Using edge

--- api/cgm.ts
+++ api/cgm.ts
@@ -1,19 +1,21 @@
 import excuteQuery from "../lib/db";
+export const runtime = "edge";
 
-export async function GET() {
+export default async function (req, res) {
+  res.setHeader("content-type", "application/json");
+  res.setHeader("Cache-Control", "public, s-maxage=1");
+  res.setHeader("CDN-Cache-Control", "public, s-maxage=10");
+  res.setHeader("Vercel-CDN-Cache-Control", "public, s-maxage=100");
   try {
     const result = await excuteQuery({
       query: "SELECT * FROM `glucose` ORDER BY `id` DESC LIMIT 1",
       values: [],
     });
-    console.log("result", result);
-    return new Response(JSON.stringify(result), {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-    });
+
+    res.status(200).json(result);
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
   }
 }
